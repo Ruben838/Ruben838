f8c222356d0f2c71eee00fc2aae3047c42fb4ca0
Ruben <Ruben@Runi878>
2023-12-05 20:18:36 +0100

    afegit hola buenas tardes, lo seran para ti doble

---
+++ Trabajos_M5_UF2/src/trabajos_m5_uf2/Trabajos_M5_UF2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajos_m5_uf2;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Trabajos_M5_UF2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
