85122c3244a5a158816482f095f1d4eee69b47a8
Ruben838 <153017060+Ruben838@users.noreply.github.com>
2023-12-16 22:12:07 +0100

    Add files via upload

---
+++ Trabajos_M5_UF2/src/trabajos_m5_uf2/Feature.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajos_m5_uf2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Feature {
+    public static double feature() {
+        Scanner sc = new Scanner(System.in);
+        double res = 0;
+        String operacion;
+        boolean comprobar = false;
+
+          String numero1;
+            do {
+                System.out.println("\n Introdueix el primer numero. ");
+                numero1 = sc.nextLine();
+            } while (!numero1.matches("[+-]?[\\d]*[.]?[\\d]+"));
+            double n1 = Double.parseDouble(numero1);
+        return n1;
+            
+}
+     public static double feature1() {
+         Scanner sc = new Scanner(System.in);
+         String numero2;
+            do {
+                System.out.println("\n Introdueix el segon numero.");
+                numero2 = sc.nextLine();
+            } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
+            double n2 = Double.parseDouble(numero2);
+        return n2;
+     }
+}
